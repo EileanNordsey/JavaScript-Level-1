74883aadea90233354ed41aaf27037cde6a6bf45
EileanNordsey <56375667+EileanNordsey@users.noreply.github.com>
2019-12-06 13:34:06 +1100

    Lesson-1
    Sorry for delay

---
+++ JavaScript-1/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let Tc = 21;
+let Tf = (9/5) * Tc + 32;
+
+alert(Tf);
+
+let name = 'Vasiliy';
+let admin = name;
+
+alert(name);
+
+alert(1000 +'108'); /* The result is unsatisfying -
+ browser thinks that 108 is just a text and adds it to the number without mathematical operation. */
+
+let message = prompt('Sensei, where is task number 3 in the homework?..');
+alert(message);
